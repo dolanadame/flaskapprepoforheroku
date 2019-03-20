a1dabdd2f587166d71155a624da782cbab21f1e4
Adam Dolan <dolanadame@github.com>
2019-03-20 19:36:53 +0100

    test

--- webapp.py
+++ webapp.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, redirect
 
-app = Flask(__name__)
+webapp = Flask(__name__)
 
-@app.route('/')
+@webapp.route('/')
 def index():
   return '<h> This is a header test </h>'
 
-@app.route('/about')
+@webapp.route('/about')
 def about():
   return render_template('about.html')
 
 if __name__ == '__main__':
-  app.run()
+  webapp.run()
