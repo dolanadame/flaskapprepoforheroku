24ab867a959cb6ce26183445da9b3ff30e572bdf
Adam Dolan <dolanadame@github.com>
2019-03-20 19:53:05 +0100

    test

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return '<h1>This is a test of the web deployment</h1>'
+  #render_template('index.html')
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+#@app.route('/about')
+#def about():
+#  return render_template('about.html')
 
 if __name__ == '__main__':
   app.run()
