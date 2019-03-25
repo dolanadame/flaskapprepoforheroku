97443e2cc7d05d16df4ec0b7ce268316caf596a3
Adam Dolan <dolanadame@github.com>
2019-03-25 23:13:05 +0100

    app.run port selection

--- app.py
+++ app.py
@@ -46,7 +46,5 @@ def my_form_post():
 		script, div=components(p)
 		return render_template('home.html', script=script, div=div)
 
-# if __name__ == '__main__':
-#   app.run(port=33507)
-
-app.run(host='0.0.0.0')
+if __name__ == '__main__':
+  app.run(port=33507)
